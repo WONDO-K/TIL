8c227c0ecee8fe0e22ab447bb05884658e727645
Wondo-K <wondok95@naver.com>
2024-02-20 14:35:33 +0900

    [TIL]&[ALGO] 2024-02-20

---
+++ algorithm/swea/2024-02-20/swea_1231.py
@@ -0,0 +1,3 @@
+import sys
+sys.stdin = open('1231_input.txt')
+sys.stdout = open('1231_output.txt','w')
---
+++ algorithm/swea/2024-02-20/swea_18542.py
@@ -0,0 +1,21 @@
+import sys
+sys.stdin = open('18542_input.txt')
+sys.stdout = open('18542_output.txt','w')
+
+def in_order(idx):
+    global num
+    if idx > n:
+        return
+    else:
+        in_order(idx*2)
+        arr[idx] = num
+        num+=1
+        in_order(idx*2+1)
+
+
+for tc in range(int(input())):
+    n = int(input())
+    arr=[0 for i in range(n+1)]
+    num=1
+    in_order(1)
+    print(f'#{tc+1} {arr[1]} {arr[n//2]}')
