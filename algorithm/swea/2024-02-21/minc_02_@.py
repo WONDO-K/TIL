563b2b5433ee8f36a1345287e9b39c5c866330ed
Wondo-K <wondok95@naver.com>
2024-02-21 23:22:57 +0900

    [ALGO] minc_02 success

--- algorithm/swea/2024-02-21/minc_02_@.py
+++ algorithm/swea/2024-02-21/minc_02_@.py
@@ -2,33 +2,28 @@ import sys
 sys.stdin = open('minc_02_input.txt')
 sys.stdout = open('minc_02_output.txt', 'w')
 
-def solve(start_i,start_j):
-    target = arr[start_i][start_j]
-    global result
-    for i in range(N):
-        for j in range(N):
-            cnt=0
-            if target == arr[i][j]:
-                for k in range(start_i,i+1):
-                    for z in range(start_j,j+1):
-                        cnt+=1
-            result.append(cnt)
-            print(f'result : {result}')
-    print(result)
-    return max(result)
-
-T = int(input())
-
-for tc in range(T):
-    N = int(input())
+def sol():
+    max_area=0
+    max_area_cnt = 0
+    for i in range(n):
+        for j in range(n):
+            for k in range(i,n):
+                for z in range(j,n):
+                    if arr[i][j] == arr[k][z]:
+                        area = (k - i + 1) * (z - j + 1)
+                        if area>max_area:
+                            max_area = area
+                            max_area_cnt = 1
+                        elif area == max_area:
+                            max_area_cnt+=1
+    return max_area_cnt
+for tc in range(int(input())):
+    arr = []
+    n = int(input())
 
     arr=[]
-    for _ in range(N):
+
+    for _ in range(n):
         arr.append(list(map(int,input().split())))
-    result=[]
-    max_area = 0
-    for i in range(N):
-        for j in range(N):
-            max_area = max(max_area,solve(i,j))
-            mac_cnt = result.count(max_area)
-    print(f'#{tc+1} {mac_cnt}')
+
+    print(f'#{tc+1} {sol()}')
