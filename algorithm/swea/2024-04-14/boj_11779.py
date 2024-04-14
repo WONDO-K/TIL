109def454c546df9c8cb8d8c6563bb76c2f7dc3d
Wondo-K <wondok95@naver.com>
2024-04-14 23:37:01 +0900

    [ALGO] 2024-04-14

--- algorithm/swea/2024-04-14/boj_11779.py
+++ algorithm/swea/2024-04-14/boj_11779.py
@@ -5,19 +5,17 @@ INF = int(1e9)
 def sol(start):
     q=[]
     heapq.heappush(q,(0,start))
-
+    distance[start]=0
     while q:
         dist,now = heapq.heappop(q)
-        if distance[now] <= dist:
-            if distance[now] == dist:
-                
-            else:
-                continue
+        if distance[now] < dist:
+            continue
 
         for i in graph[now]:
             cost = dist + i[1]
             if cost < distance[i[0]]:
                 distance[i[0]] = cost
+                graph2[i[0]]=now
                 heapq.heappush(q,(cost,i[0]))
 
 n = int(input())
@@ -25,6 +23,7 @@ m = int(input())
 
 graph = [[]for i in range(n+1)]
 distance = [INF]*(n+1)
+graph2 = [0] * (n+1)
 
 for _ in range(m):
     a,b,c = map(int,input().split())
@@ -33,14 +32,12 @@ for _ in range(m):
 start,end = map(int,input().split())
 
 sol(start)
+print(distance[end])
 
 result = []
-for i in range(1, n+1):
-    if distance[i] == distance[end]: 
-        result.append(i)
 
-
-
-print(distance[end])
-print(f'dist: {distance}')
-print(result)
+while end:
+    result.append(end)
+    end = graph2[end]
+print(len(result))
+print(*result[::-1])
