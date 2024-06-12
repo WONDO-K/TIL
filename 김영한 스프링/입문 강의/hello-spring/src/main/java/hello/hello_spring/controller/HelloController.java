95237928b5d7a7497f19d8cf71a127ab856082b5
WONDO-K <wondok95@naver.com>
2024-06-12 18:12:31 +0900

    [TIL] 2024-06-12

--- 김영한 스프링/입문 강의/hello-spring/src/main/java/hello/hello_spring/controller/HelloController.java
+++ 김영한 스프링/입문 강의/hello-spring/src/main/java/hello/hello_spring/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -18,5 +19,32 @@ public class HelloController {
     public String helloMvc(@RequestParam("name") String name, Model model) {
         model.addAttribute("name", name);
         return "hello-template";
+    }
+    @GetMapping("hello-string")
+    @ResponseBody // http에서 헤더, 바디 중 바디에 "hello " + name; 이 정보를 바로 넣는다는 의미
+    public String helloString(@RequestParam("name") String name){
+        return "hello " + name;
+    }
+
+    // json 방식으로 출력된다.
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name){
+        Hello hello = new Hello(); // 객체
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello{
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
     }
 }
---
+++ 김영한 스프링/입문 강의/hello-spring/src/main/java/hello/hello_spring/repository/MemberRepository.java
@@ -0,0 +1,13 @@
+package hello.hello_spring.repository;
+
+import hello.hello_spring.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+    Member save(Member member);
+    Optional<Member> findById(Long id); //Optional java8의 문법으로 findBy로 정보를 가져올 때 null일 수 있는데 Optional로 감싸서 리턴한다.(null 처리)
+    Optional<Member> findByName(String name);
+    List<Member> findAll();
+}
