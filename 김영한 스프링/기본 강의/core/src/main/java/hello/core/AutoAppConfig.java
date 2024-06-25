87e6d4bfee61e3ce0db6c0b34d222ebb4417f0ba
WONDO-K <wondok95@naver.com>
2024-06-26 01:12:41 +0900

    [TIL] 2024-06-26

---
+++ 김영한 스프링/기본 강의/core/src/main/java/hello/core/AutoAppConfig.java
@@ -0,0 +1,14 @@
+package hello.core;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class) // 컴포넌트 스캔에서 제외할 항목
+        // AppConfig의 @Configuration은 수동을 등록하기 때문에 자동으로 등록해버리면 안된다. 그리고 원래 @Component가 붙어 있어서 스캔 대상임
+) // @ComponentScan는 스프링 빈을 자동으로 끌어올려야함, @Component 어노테이션이 붙어 있는 클래스를 찾아서 자동으로 스프링 빈에 등록해준다.
+public class AutoAppConfig {
+
+}
--- 김영한 스프링/기본 강의/core/src/main/java/hello/core/member/MemberServiceImpl.java
+++ 김영한 스프링/기본 강의/core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,10 +1,16 @@
 package hello.core.member;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class MemberServiceImpl implements MemberService{
 
     private final MemberRepository memberRepository; // 추상화에만 의존 AppConfig에서 생성자 주입
     
     // 생성자를 통해 어떤 객체를 주입할지 정해야함
+    @Autowired // MemberRepository타입에 맞는 클래스를 찾아와서 자동으로 연결해서 주입해준다.
+    // ac.getBean(MemberRepository.class) 처럼 동작한다. (다른 동작들이 더 있긴 하지만)
     public MemberServiceImpl(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
