9f08822bc19fdc5e5a8feed35b7181e0109698cb
WONDO-k <wondok95@naver.com>
2025-01-06 20:56:11 +0900

    [TIL] 2025-01-06

---
+++ algorithm/boj_9251_LIS.py
@@ -0,0 +1,14 @@
+n = list(str(input()))
+m = list(str(input()))
+
+dp = [[0 for _ in range(len(n)+1)] for _ in range(len(m)+1)]
+
+for i in range(1,len(m)+1):
+    for j in range(1,len(n)+1):
+
+        if m[i-1] == n[j-1]:
+            dp[i][j] = dp[i-1][j-1]+1
+        else:
+            dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+
+print(dp[len(m)][len(n)])
